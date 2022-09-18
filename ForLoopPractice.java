9eb23417a64749c5796abdfb4266ac499321bcf4
Chintan Patel <Chintanpatel7776@gmail.com>
2022-09-18 14:58:34 +0100

    SepLearning

---
+++ ForLoopPractice.java
@@ -0,0 +1,20 @@
+package SepLearning;
+//nested for loop
+public class ForLoopPractice {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= 8; j++) {
+
+                // for (int j = 0; j <=8; j++)
+                System.out.print(i+" ");
+                System.out.println(j+" ");
+            }
+            // System.out.println();
+            //System.out.println("=====================");
+            //for (int i = 0; i <=8 ; i++)
+            //for (int j = 0; j <=8; j++)
+            // System.out.print(i+" ");
+        }
+    }
+}
---
+++ SortArraysPractice.java
@@ -0,0 +1,13 @@
+package SepLearning;
+import java.util.Arrays;
+
+public class SortArraysPractice {
+    public static void main(String[] args) {
+        int[] nums= {12,4,3,1,2,8,5,0};
+        Arrays.sort(nums);
+        for (int i:nums) {
+            System.out.print(i+" ");
+        }
+    }
+
+}
